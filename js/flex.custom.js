af5c0cac7465619f0bc45ffc01484e573f0feceb
Kevin Carwile <kcarwile@gmail.com>
2014-08-17 19:17:31 -0700

    now using the contact form feature built into the flex framework

--- js/flex.custom.js
+++ js/flex.custom.js
@@ -42,45 +42,6 @@ flexloader.execute(function ($, App) {
         }
     });
 
-    /* Contact Form Wiring */
-    var updateContactForm = function () {
-        $.get('//www.beatbrokerz.com/contact_form/' + App.appSettings.acct_id, { ajax: true }, function (data) {
-            var content = $('<div>').html(data);
-            var form_build_id = content.find('input[name="form_build_id"]').val();
-            var form_token = content.find('input[name="form_token"]').val();
-            $('.modal-email-dialog input[name="form_build_id"]').attr({ id: form_build_id, value: form_build_id });
-            $('.modal-email-dialog input[name="form_token"]').val(form_token);
-            if (content.find('.captcha').length) {
-                $('.modal-email-dialog div.reCaptcha').html(content.find('.captcha').html());
-                Recaptcha.create('6LdQ3cESAAAAANkmtIRMUduWPL06DtEWI4XmU_v2', 'recaptcha_ajax_api_container', {theme: 'clean'});
-            }
-        }, 'jsonp');
-    }
-    $(document).on('click', '#email-modal', updateContactForm);
-    $('#modal-contact-form').ajaxForm({
-        clearForm: false,
-        beforeSubmit: function () {
-            $("#modal-contact-form").activity();
-        },
-        success: function (response, status) {
-            $("#modal-contact-form").activity(false);
-            var content = $('<div>').html(response.replace(/src=/gi, 'nosrc='));
-            var message = content.find('#system-messages .messages');
-            if (message.hasClass('error')) {
-                alertify.error(message.text());
-            }
-            else {
-                alertify.success(message.text());
-                $('#modal-contact-form')[0].reset();
-            }
-            updateContactForm();
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            $("#modal-contact-form").activity(false);
-            alertify.error('There was an error sending your message.');
-        }
-    });
-
     /**
      *  Some of our interface items depend on app data to be ready to display correctly,
      *  so we wrap that code with App.ready()
